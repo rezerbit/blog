9aacd4fc1474239f5e5a0f2cf12bee0de2f1853b
rezerbit <rezerbit@gmail.com>
2015-04-07 11:29:49 +0500

    ..

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
 
+  def show
+    @article = Article.find(params[:id])
   end
 
   def new
@@ -8,9 +12,16 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
 
     @article.save
     redirect_to @article
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
