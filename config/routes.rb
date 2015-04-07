ddd0eff89358741436d25b51a0d473de9eb291ce
rezerbit <rezerbit@gmail.com>
2015-04-07 17:04:45 +0500

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
